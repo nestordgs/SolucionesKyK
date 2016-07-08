33b0301731529860139248c2b33a4a355ee93a44
Nestordgs <nestordgs@hotmail.com>
2016-07-08 00:29:27 -0400

    Nestor Gutierrez T:24
    Se realizaron algunas mejoras con respecto a los estilos:
    1. Se agregaron los estilos para el footer del menu, en el cual se muestra la direccion, el correo electronico y las redes sociales.
    2. Se modificaron los estilos de "hover" al momento de estar sobre cada link, el mismo consite en una linea inferior que cree de manera exponencial hasta ocupar un 88% del link.
    3. Se redujo el tamaño de la letra en cada uno de los link, con la finalidad de hacer un poco mas pequeño el menu.
    4. Se modifico el Boton del menu Desktop para que no tuviese que hacerse click especificamente sobre las lineas sino sobre un cuadro imaginario en el cual el se encuentra.
    5. Se retiro el borde de los cuadros de quienes somos?, mision y vision de la pagina principal.
    6. Se desplazo un poco la imagen de Seguridad Industrial para que las letras no esten sobre el rostro de la persona sino por debajo.
    7. Se agrego una nueva clase llamada "Fail" la cual es utilizada en los input del formulario de contacto.
    8. Se termino la funcion para validar los el formulario de contacto, en el mismo se valida que los input no sean vacios y a su vez se evalua mediante una expresion regular que esten escritos de manera correcta los datos de cada input.
    9. Se agregaron las etiqueta meta.
    10. Se agrego la funcion para utilizar Google Analytic.
    11. Se modifico el tamaño de los titulos "¿Quienes Somos?, Mision y Vision" ya que en mobiles no se veian de manera correcta dichos cuadros.
    12. Se Agregaron nuevos modales por cada micro servicio.

--- js/soluciones.js
+++ js/soluciones.js
@@ -1,9 +1,4 @@
-$("#btn-menu-mobile").click(function(){
-	if ($("#menu-mobile").hasClass("open")) {
-		$("#menu-mobile").removeClass("open");
-	}else{
-		$("#menu-mobile").addClass("open");
-	}
+$("#btn-menu-mobile").click(function(){if ($("#menu-mobile").hasClass("open")) {$("#menu-mobile").removeClass("open");}else{$("#menu-mobile").addClass("open");}
 });
 $("#btn-menu-desktop").click(function(){
 	if ($("#nav-bar-desktop").hasClass("open")) {
@@ -27,15 +22,15 @@ document.querySelector( "#btn-menu-mobile" ).addEventListener( "click", function
 	this.classList.toggle( "active" );
 });
 
-$("#sis_gestion").click(function() {
+/*$("#sis_gestion").click(function() {
 	if ($("#perolero").hasClass('open-info')) {
 		$("#perolero").removeClass('open-info');
 	} else {
 		$("#perolero").addClass('open-info');
 	}
-});
+});*/
 
-
+window.onload = function(){$("#div-logo").addClass("loaded");setTimeout(function(){$('body').addClass("loaded")},7000)}
 // Detectando SCROLL
 
 /*var sv = $("#serv_admin").scroll(function(){
@@ -57,3 +52,126 @@ $(document).ready(function(){
 	});
 
 })*/
+$('#contact-form').submit(function (event) {
+	event.preventDefault()
+});
+
+var validateForm;
+var Name = true;
+var Email = true;
+var Phone = true;
+var Consulta = true;
+var Comment = true;
+var httpC;
+validateForm = function () {
+
+	$('input').each(function () {
+            var inputName = $(this).attr('id');
+            switch (inputName) {
+                case 'name':
+                    if (!$(this).val().match(/[A-Za-z0-9 .áéíóúñ]{10,50}/) || $(this).val() == '') {
+                        $(this).addClass('fail');
+                        Name = false;
+                    } else {
+                        $(this).removeClass('fail');
+                        Name = true;
+                    }
+                    break;
+                case 'email':
+                    if (!$(this).val().match(/^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/) || $(this).val() == '') {
+                        $(this).addClass('fail');
+                        Email = false;
+                    } else {
+                        $(this).removeClass('fail');
+                        Email = true;
+                    }
+                    break;
+                case 'phone':
+                    if (!$(this).val().match(/^[0-9-()+]{7,20}/) || $(this).val() == '') {
+                        $(this).addClass('fail');
+                        Phone = false;
+                    } else {
+                        $(this).removeClass('fail');
+                        Phone = true;
+                    }
+					break;
+				default:
+					$(this).addClass('fail');
+			}
+	});
+    if ($("#consulta").val()==""){
+        $("#consulta").addClass('fail');
+        Consulta = false;
+    }else {
+        $("#consulta").removeClass('fail');
+        Consulta = true;
+    }
+    if ($("#comment").val() == "" || !$("#comment").val().match(/^[A-Za-z 0-9.,&?!\*\n\+\/-]{10,350}/)) {
+        $("#comment").addClass('fail');
+        Comment = false;
+    } else {
+        $("#comment").removeClass('fail');
+        Comment = true;
+    }
+
+    if (Name == true && Email == true && Phone == true && Consulta == true && Comment == true){
+        $.ajax({
+            type: 'POST',
+            url: $('#contact-form').attr('action'),
+            data: 'name='+$('#name').val()+'&email='+$('#email').val()+'&phone='+$('#phone').val()+'&consulta='+$('#consulta').val()+'&comment='+$('#comment').val()+"&g-recaptcha-response=" + grecaptcha.getResponse(),
+            dataType: "json",
+            beforeSend: function(datax){
+                if(httpC){
+                    // Si habia alguna conexion anterior, la cancelamos
+                    httpC.abort();
+                }
+                httpC = datax;
+            },
+            success: function(data) {
+                if (data.error) {
+                    $("#div_alerts").append(data.error);
+                }
+                else{
+                    $('input').each(function () {
+                        $(this).val('');
+                    });
+                    $('#consulta').val('');
+                    $('#comment').val('');
+                }
+            }
+        })
+    }
+
+};
+
+/*var divError = $('#message-error');
+var formData = $('input').each(function(){$(this).val();});
+
+$('#send').click(function () {
+    $.ajax({
+        type: 'POST',
+        url: $('#contact-form').attr('action'),
+        data: formData
+    }).done(function (response) {
+        divError.removeClass('error');
+        divError.addClass('success');
+
+        divError.text(response);
+
+        $('#name').val('');
+        $('#email').val('');
+        $('#phone').val('');
+        $('#comment').val('');
+        $('#type_cons').val('');
+
+    }).fail(function (data) {
+        divError.removeClass('success');
+        divError.addClass('error');
+
+        if (data.responseText !== ''){
+            divError.text(data.responseText);
+        }else{
+            divError.text('Oops! Parece que algo ha salido muy mal');
+        }
+    });
+});*/
